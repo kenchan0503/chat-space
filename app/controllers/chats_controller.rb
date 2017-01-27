7b06fb1151d162288d617d8e425d35bba665b9d9
Timegoesveryswiftly <kawaikatsuharu@27-139-68-86.rev.home.ne.jp>
2017-01-27 18:29:44 +0900

    5th user_manage_function_mount check over on L9

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,18 +2,5 @@ class ChatsController < ApplicationController
   def index
     @chats = Chat.all
   end
-  def new
-  end
-  def create
-    Chat.create(chat_params)
-  end
-  def destroy
-  end
-
-  private
-
-  def chats_params
-    params.permit(:name, :body)
-  end
   
 end
